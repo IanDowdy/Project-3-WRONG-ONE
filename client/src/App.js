35780969ac00f9decb4308153787f50eb40feaf3
Ian <ian_dowdy@outlook.com>
2018-12-22 13:09:58 -0600

    connected express server

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Connection from './components/Connection';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Nicole, Pat, Jennifer, Alex, and Ian.... WHAT UP FOOOS!</h1>
+        <Connection />
       </div>
     );
   }
