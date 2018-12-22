8b71a9ba20c6fe3581069ef74ecefc7e23042f69
Ian <ian_dowdy@outlook.com>
2018-12-22 11:41:44 -0600

    barebones react wireframe

---
+++ project3/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Nicole, Pat, Jennifer, Alex, and Ian.... WHAT UP FOOOS!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
