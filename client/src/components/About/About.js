eb851255be2875929c74f1e353a265b41381f458
Pat Seade <pattimint6@gmail.com>
2019-01-16 16:12:48 -0600

    css

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Connection from "../Connection/Connection";
 
 
 import './About.css';
@@ -6,10 +7,15 @@ import './About.css';
 class About extends Component {
   render() {
     return (
+      <div>
+      
       <div className="App">
        <h1>Who we are, what we're about, what this app is and</h1>
          <h2 id="title">what we hope to accomplish with it.</h2>
+         <Connection />
+
        
+      </div>
       </div>
     );
   }
