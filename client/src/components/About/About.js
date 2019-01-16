03912aceec0cd72c94aa5675c6d5434ca85d356e
Pat Seade <pattimint6@gmail.com>
2019-01-16 12:55:48 -0600

    test

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -7,7 +7,7 @@ class About extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Who we are, what we're about, what this app is and what we hope to accomplish with it.</h1>
+       <h1>Who we are, what we're about, what this app is and what we hope to accomplish with it..</h1>
        
       </div>
     );
