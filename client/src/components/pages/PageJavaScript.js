8791373aa410d231aff2c356527244c1ecc0d92a
Pat Seade <pattimint6@gmail.com>
2019-01-16 23:27:15 -0600

    added pages

---
+++ client/src/components/pages/PageJavaScript.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class PageJavaScript extends React.Component {
+    render() {
+        return (
+            <h1>JavaScript</h1>
+        );
+    }
+}
+
+export default PageJavaScript;
