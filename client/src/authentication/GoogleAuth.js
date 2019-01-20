09a45b009f05329ef2b3f243ced92bde1d78b28d
Pat Seade <pattimint6@gmail.com>
2019-01-20 16:17:10 -0600

    authentication

--- client/src/authentication/GoogleAuth.js
+++ client/src/authentication/GoogleAuth.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-        state = { isSignedIn: null };
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -9,13 +10,17 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
-onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+onAuthChange = (isSignedIn) => {
+    if (isSignedIn) { 
+        this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+        this.props.signOut();
+    }
 };
 
 onSignInClick = () => {
@@ -27,9 +32,9 @@ onSignOutClick = () => {
 }
 
     renderAuthButton() {
-        if(this.state.isSignedIn === null) {
+        if(this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOutClick} className="ui red google button">
                 <i className="google icon" />
@@ -56,5 +61,10 @@ onSignOutClick = () => {
 };
 
 
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+};
 
-export default GoogleAuth;
+export default connect(
+    mapStateToProps,
+     { signIn, signOut }) (GoogleAuth);
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+<App />
+</Provider>, 
+document.getElementById('root')
+);
 
