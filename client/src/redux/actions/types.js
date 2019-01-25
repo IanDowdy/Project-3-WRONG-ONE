1e994030d62ccc90bbf1413272da4e37a90da4b8
Pat Seade <pattimint6@gmail.com>
2019-01-24 19:15:17 -0600

    folder addition & css

---
+++ client/src/redux/actions/types.js
@@ -0,0 +1,7 @@
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const ITEMS_LOADING = 'ITEMS_LOADING';
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+
