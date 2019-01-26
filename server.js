333edc889c1f616d0bc7e7bf71e6e3690b658e89
IanDowdy <40776425+IanDowdy@users.noreply.github.com>
2019-01-26 12:47:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(app.use(express.static(path.join(__dirname, '/build')));
 }
 
+app.get('*', (req, res) => {	
+        res.sendFile(path.join(__dirname, './client/build/public/index.html'));	
+    });
+            
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
