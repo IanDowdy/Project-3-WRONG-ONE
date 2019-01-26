dbb63e43528f60542da5bce6956e82004056d0e3
jenn210 <jennifer.c.tuten@gmail.com>
2019-01-25 22:02:10 -0600

    public/private routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,28 +12,53 @@ import PageJscript from './components/pages/PageJscript';
 import Login from './components/Login/Login';
 import Learn from './components/Learn/Learn';
 import Resources from './components/Resources/Resources';
- 
-class App extends Component {
+
+class App extends React.Component {
+
   render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <div>
-            <Navigation />
-            <Switch> 
-       <Route path="/" component={Home} exact />
-       <Route path="/about" component={About} />
-       <Route path="/freecode" component={FreeCode} />
-       <Route path="/learn" component={Learn}/>
-       <Route path="/login" component={Login}/>
-       <Route path="/jscript" component={PageJscript}/>
-       <Route path="/resources" component={Resources}/>
-       <Route path="" component={Error} />
-     </Switch>
-     </div>
-     </BrowserRouter>
-     </div>
-    );
+    console.log('Logged In? ',this.props.isSignedIn)
+    if (this.props.isSignedIn) {
+      return (
+        <div>
+          <BrowserRouter>
+            <div>
+              <Navigation />
+              <Switch> 
+         <Route path="/" component={Home} exact />
+         <Route path="/about" component={About} />
+         <Route path="/freecode" component={FreeCode} />
+         <Route path="/learn" component={Learn}/>
+         <Route path="/login" component={Login}/>
+         <Route path="/jscript" component={PageJscript}/>
+         <Route path="/resources" component={Resources}/>
+         <Route path="" component={Error} />
+       </Switch>
+       </div>
+       </BrowserRouter>
+       </div>
+      );
+    }
+    else {
+      return (
+        <div>
+          <BrowserRouter>
+            <div>
+              <Navigation />
+              <Switch> 
+         <Route path="/" component={Home} exact />
+         <Route path="/about" component={About} />
+         <Route path="/freecode" component={Login} />
+         <Route path="/learn" component={Login}/>
+         <Route path="/login" component={Login}/>
+         <Route path="/jscript" component={Login}/>
+         <Route path="/resources" component={Login}/>
+         <Route path="" component={Error} />
+       </Switch>
+       </div>
+       </BrowserRouter>
+       </div>
+      )
+    }
   }
 }
 
---
+++ client/src/components/PrivateRoutes/PublicRoutes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import Footer from '../Footer/Footer';
+//import './Learn.css';
+//import Exercises from '../Exercises/Exercises'
+
+class PublicRoutes extends Component {
+  render() {
+    return (
+      <div>
+
+      <p>This is a Public Route</p>
+
+      </div>
+    );
+  }
+}
+
+export default PublicRoutes;
