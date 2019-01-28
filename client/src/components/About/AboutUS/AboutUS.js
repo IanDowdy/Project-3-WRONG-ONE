b8ebf888cb65bcec9af1aae99987ce0edc1ef833
artooth <artooth@gmail.com>
2019-01-27 18:04:32 -0600

    about page components

---
+++ client/src/components/About/AboutUS/AboutUS.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+
+const AboutUS = (props) => {
+    return (
+
+        <Container>
+            <Row>
+
+                <Col>
+                    <p>
+                        We are a team of coders who come from all different backgrounds. We understand the challenges of learning coding, and being a newbie in the tech industry, which is why we decided to help others on their coding journey. We came together to start App a Day! which is a new way for beginning coders to enter a calm and friendly atmosphere to start learning the fundamentals of coding. By taking small functional pieces of code and breaking them down line by line after being replicated we hope to give users an easily digestible set of tools to grow as programmer.
+                    </p>
+                </Col>
+
+
+                <Col>
+
+                </Col>
+
+
+
+            </Row>
+
+        </Container>
+    );
+};
+
+export default AboutUS;
---
+++ client/src/components/About/Team/Team.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import './Team.css';
+
+const Team = (props) => {
+    return (
+
+        <Container>
+            <Row-1>
+
+                <Col>
+
+                </Col>
+
+            </Row-1>
+            <Row-2>
+
+                <Col>
+
+                </Col>
+
+            </Row-2>
+            <Row-3>
+
+                <Col>
+
+                </Col>
+
+            </Row-3>
+            <Row-4>
+
+                <Col>
+
+                </Col>
+
+            </Row-4>
+            <Row-5>
+
+                <Col>
+
+                </Col>
+
+            </Row-5>
+
+
+
+
+
+
+        </Container>
+    );
+};
+
+export default Team;
