f2d1deb3ced69cd12faebba981a8e866eb89234e
IanDowdy <40776425+IanDowdy@users.noreply.github.com>
2019-01-29 13:55:09 -0600

    Update server.js
    That get '*' keeps reappearing and i think it's what's causing the crash on heroku

--- server.js
+++ server.js
@@ -32,10 +32,6 @@ app.use(routes);
 if (process.env.NODE_ENV === 'production') {
     app.use(app.use(express.static(path.join(__dirname, '/build')));
 }
-
-app.get('*', (req, res) => {	
-        res.sendFile(path.join(__dirname, './client/build/public/index.html'));	
-    });
             
 const port = process.env.PORT || 5000;
 
