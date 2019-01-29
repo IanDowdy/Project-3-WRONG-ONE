16c97ef030efbe5bbe31de61cdbd5d2f8e123156
jenn210 <jennifer.c.tuten@gmail.com>
2019-01-29 00:39:09 -0600

    add lessons

--- client/src/components/Lessons/Lesson3.js
+++ client/src/components/Lessons/Lesson3.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './Lessons.css';
 
-class Lesson1 extends Component {
+class Lesson3 extends Component {
     render() {
         return (
             <div>
                 <div className="ui container">
                 <div className="ui equal width center aligned padded grid">
       <div className="column"></div>
-      <div className="lesson-header"><h1>Lesson 1: Arrays</h1></div>
+      <div className="lesson-header"><h1>Lesson 3: Functions</h1></div>
          <div className="lesson-container">    
-         <img className="lesson-pics" src="../../assets/images/num6.png" style={{ width: 600 }}/>
-      <iframe className="editor-container" src="https://stackblitz.com/edit/js-5orkrc?embed=1&file=index.js" view="Editor" title="code editor"></iframe>
+         <img className="lesson-pics" src="../../assets/images/functions.png"/>
+      <iframe className="editor-container" src="https://stackblitz.com/edit/js-vkar9c?embed=1&file=index.js" view="Editor" title="code editor"></iframe>
                 </div>
                 </div>
             </div>
@@ -24,4 +24,4 @@ class Lesson1 extends Component {
 }
 
 
-export default Lesson1;
+export default Lesson3;
